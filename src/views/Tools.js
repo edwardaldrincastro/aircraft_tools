a48d176f5536858d9040e145f3b650f0ba075fa4
renz <castalonirenz@gmail.com>
2019-01-04 16:18:29 +0800

    modal in list of tools

--- src/views/Tools.js
+++ src/views/Tools.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, View, ScrollView, Image, ActivityIndicator, NetInfo } from 'react-native'
+import { StyleSheet, Text, View, ScrollView, Image, ActivityIndicator, NetInfo, Modal, TouchableOpacity, Alert, Button } from 'react-native'
 
 const axios = require('axios')
 
@@ -7,8 +7,18 @@ class Tools extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            tools: []
+            tools: [],
+            modalVisible: false,
+                name: '',
+                img: null
+
         };
+    }
+    setModalVisible =(name,img) =>{
+        this.setState({ modalVisible: true,
+                name:name,
+                img: img
+             })
     }
     async componentDidMount() {
         const response = await axios.get('https://aircraftmaintenance-350da.firebaseio.com/tools.json')
@@ -31,14 +41,32 @@ class Tools extends Component {
         myState = this.state.tools
         return (
             <View style={styles.container}>
+                <Modal
+                    animationType="slide"
+                    transparent={false}
+                    visible={this.state.modalVisible}
+                    onRequestClose={() => {
+                       this.setState({modalVisible: false});
+                    }}>
+                    <View style={{ marginTop: 22 }}>
+                        <View style={{alignItems:"center", justifyContent:"space-around"}}>
+                            <Text>{this.state.name}</Text>
+                            <Image source={{uri: this.state.img}} style={{height:"80%", width: "80%"}}/>
+                            <Button title="Close" onPress={()=>this.setState({modalVisible:false})}></Button>
+                        </View>
+                    </View>
+                </Modal>
                 <ScrollView>
-                    <Text style={styles.welcome}>List of all tools</Text>
+                    <Text style={styles.welcome}>List of Tools</Text>
                     {status ? myState.map((item, index) => {
                         return (
-                            <View key={index} style={styles.renderItem}>
+                            <TouchableOpacity key={index} onPress={this.setModalVisible.bind(this, item.name, item.image)}>
+                            <View  style={styles.renderItem}>
                                 <Text>{item.name}</Text>
                                 <Image source={{ uri: item.image }} style={{ height: 50, width: 50 }} />
                             </View>
+                            </TouchableOpacity>
+                          
                         )
                     })
                         : <ActivityIndicator />}
