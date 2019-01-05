2df9828c6b5fcd56f9d31c800bb982eff5e37a25
renz <castalonirenz@gmail.com>
2019-01-05 17:01:24 +0800

    lutang haha

--- src/views/Tools.js
+++ src/views/Tools.js
@@ -9,16 +9,19 @@ class Tools extends Component {
         this.state = {
             tools: [],
             modalVisible: false,
-                name: '',
-                img: null
+            name: '',
+            img: null,
+            description: ''
 
         };
     }
-    setModalVisible =(name,img) =>{
-        this.setState({ modalVisible: true,
-                name:name,
-                img: img
-             })
+    setModalVisible = (name, img, description) => {
+        this.setState({
+            modalVisible: true,
+            name: name,
+            img: img,
+            description: description
+        })
     }
     async componentDidMount() {
         const response = await axios.get('https://aircraftmaintenance-350da.firebaseio.com/tools.json')
@@ -46,28 +49,31 @@ class Tools extends Component {
                     transparent={false}
                     visible={this.state.modalVisible}
                     onRequestClose={() => {
-                       this.setState({modalVisible: false});
+                        this.setState({ modalVisible: false });
                     }}>
-                    <View style={{ marginTop: 22 }}>
-                        <View style={{alignItems:"center", justifyContent:"space-arogund"}}>
-                            <Text>{this.state.name}</Text>
-                            <Text>{this.state.description}</Text>
-                            <Image source={{uri: this.state.img}} style={{height:"80%", width: "80%"}}/>
-                            <Button title="Close" onPress={()=>this.setState({modalVisible:false})}></Button>
-                      </View>
-                      </View>
+
+                    <View style={{ flex: 1, alignItems: "center", justifyContent: "space-around" }}>
+                        <Text style={{ fontSize: 26, fontWeight: "bold", color: "#000" }}>Tool Details:</Text>
+                        <Text style={[styles.detailText, {textDecorationLine:"underline", fontWeight:"bold"}]}>{this.state.name}</Text>
+                        <View style={{ width: "90%", backgroundColor:"#fff", borderRadius:25, elevation: 20 }}>
+                            <Text style={styles.detailText}>{this.state.description}</Text>
+                        </View>
+                        <Image source={{ uri: this.state.img }} style={{ height: "50%", width: "80%" }} />
+                        <Button title="Close" onPress={() => this.setState({ modalVisible: false })}></Button>
+
+                    </View>
                 </Modal>
                 <ScrollView>
                     <Text style={styles.welcome}>List of Tools</Text>
                     {status ? myState.map((item, index) => {
                         return (
-                            <TouchableOpacity key={index} onPress={this.setModalVisible.bind(this, item.name, item.image)}>
-                            <View  style={styles.renderItem}>
-                                <Text>{item.name}</Text>
-                                <Image source={{ uri: item.image }} style={{ height: 50, width: 50 }} />
-                            </View>
+                            <TouchableOpacity key={index} onPress={this.setModalVisible.bind(this, item.name, item.image, item.description)}>
+                                <View style={styles.renderItem}>
+                                    <Text>{item.name}</Text>
+                                    <Image source={{ uri: item.image }} style={{ height: 50, width: 50 }} />
+                                </View>
                             </TouchableOpacity>
-                          
+
                         )
                     })
                         : <ActivityIndicator />}
@@ -98,6 +104,11 @@ const styles = StyleSheet.create({
         color: '#000',
         margin: 15,
     },
+    detailText: {
+        textAlign:"center",
+        fontSize: 18,
+        color: "#000"
+    }
 })
 
 export default Tools
